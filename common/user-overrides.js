8f3f1e602f7fb013a8479f0c41cc73bc751c2e0d
6e6f61 <109554668+6e6f61@users.noreply.github.com>
2022-11-14 21:30:50 +1030

    Add Arkenfox overrides to common

---
+++ common/user-overrides.js
@@ -0,0 +1,12 @@
+user_pref("browser.compactmode.show", true);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.startup.page", 3); // Enable session restore
+user_pref("keyword.enabled", true);
+user_pref("privacy.resistFingerprinting.letterboxing", false);
+user_pref("network.http.referer.XOriginPolicy", 0);
+user_pref("media.rdd-ffmpeg.enabled", true);
+user_pref("media.ffmpeg.vaapi.enabled", true);
+user_pref("media.navigator.mediadataencoder_vpx_enabled", true);
+user_pref("media.ffvpx.enabled", false);
+user_pref("media.rdd-vpx.enabled", false);
+user_pref("media.rdd-process.enabled", false);
